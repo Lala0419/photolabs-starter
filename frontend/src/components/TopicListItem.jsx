f87862c3e65b2728dc232dea3f54b11e475ccd12
Nanaya <nanayamiki@gmail.com>
2023-08-18 22:27:30 -0700

    delete the fake data and add pros

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,18 +2,13 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
-  return (
-    <div className="topic-list__item">
-      {/* Insert React */}
-    </div>
-  );
+const TopicListItem = (props) => {
+	//console.log("props", props);
+	return (
+		<div className="topic-list__item">
+			<span>{props.topic.title}</span>
+		</div>
+	);
 };
 
 export default TopicListItem;
