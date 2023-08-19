2fb2daf8b676657b18a43ce8924dd794985f4013
Nanaya <nanayamiki@gmail.com>
2023-08-19 16:43:50 -0700

    delete sample data and import mockup data

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import '../styles/HomeRoute.scss';
+import "../styles/HomeRoute.scss";
+import TopNavigation from "components/TopNavigationBar";
+import PhotoList from "components/PhotoList";
 
-const HomeRoute = () => {
-  return (
-    <div className="home-route">
-      {/* Insert React */}
-    </div>
-  );
+const HomeRoute = (props) => {
+	return (
+		<div className="home-route">
+			<TopNavigation topicData={props.topicData} />
+			<PhotoList photoData={props.photoData} />
+		</div>
+	);
 };
 
 export default HomeRoute;
