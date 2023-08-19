a25268b76d7400ea0aaecd6751603793a9d3e63f
Nanaya <nanayamiki@gmail.com>
2023-08-18 20:25:53 -0700

    remove photoListItem and add photoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+import "./App.scss";
+import PhotoList from "components/PhotoList";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <div className="App container">{photos}</div> */}
+			<PhotoList />
+		</div>
+	);
 };
 
 export default App;
