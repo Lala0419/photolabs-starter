7501a2575034ba93c81e730853f1c82409ee0d07
Nanaya <nanayamiki@gmail.com>
2023-08-18 20:27:55 -0700

    add useState for toggling like favicon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,16 +1,21 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
-  return (
-    <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
-      </div>
-    </div>
-  );
+	const [toggle, setToggle] = useState(false);
+
+	const clickHandler = () => {
+		setToggle((prev) => !prev);
+	};
+	return (
+		<div className="photo-list__fav-icon">
+			<div className="photo-list__fav-icon-svg" onClick={clickHandler}>
+				{toggle && FavIcon}
+			</div>
+		</div>
+	);
 }
 
 export default PhotoFavButton;
