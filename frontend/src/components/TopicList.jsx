ae2f5496d9919b99be3ee2d0a011ef04c976b033
Nanaya <nanayamiki@gmail.com>
2023-08-18 22:29:25 -0700

    update the component and import TopicListItem component

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
 const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
+	{
+		id: "1",
+		slug: "topic-1",
+		title: "Nature",
+	},
+	{
+		id: "2",
+		slug: "topic-2",
+		title: "Travel",
+	},
+	{
+		id: "3",
+		slug: "topic-3",
+		title: "People",
+	},
 ];
 
 const TopicList = () => {
-  return (
-    <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
-    </div>
-  );
+	const topics = sampleDataForTopicList.map((item) => (
+		<TopicListItem key={item.id} topic={item} />
+	));
+	return <div className="top-nav-bar__topic-list">{topics}</div>;
 };
 
 export default TopicList;
