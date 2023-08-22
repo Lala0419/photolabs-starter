69c56bb2c080722e5804f8b965f767f73a42536c
Nanaya <nanayamiki@gmail.com>
2023-08-21 18:28:49 -0700

    add Modal component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import "../styles/HomeRoute.scss";
 import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
+import Modal from "../routes/PhotoDetailsModal";
 
 const HomeRoute = (props) => {
 	const [isFavorite, setIsFavorite] = useState([]);
+	const [modal, setModal] = useState(false);
+	const [photoDetail, setPhotoDetail] = useState({});
 
 	const toggleFavorite = (photoId) => {
 		if (isFavorite.includes(photoId)) {
@@ -14,29 +17,22 @@ const HomeRoute = (props) => {
 		}
 	};
 
-	// const displayHandler = () => {
-	// 	console.log(display);
-	// 	setDisplay((prev) => prev + 1);
-	// };
-
-	// const displayHandler = (props) => {
-	// 	props === "plus" && setDisplay((prev) => prev + 1);
-	// 	props === "minus" && setDisplay((prev) => prev - 1);
-	// };
+	const toggleModal = (photoId) => {
+		setModal((prev) => !prev);
+		setPhotoDetail(props.photoData[photoId - 1]);
+		console.log("photoDetail", props.photoData[photoId - 1]);
+	};
 
 	return (
 		<div className="home-route">
-			{/* <TopNavigation topicData={props.topicData} display={display} />
-			<PhotoList
-				photoData={props.photoData}
-				displayHandler={displayHandler}
-				setModal={props.setModal} */}
 			<TopNavigation topicData={props.topicData} isFavorite={isFavorite} />
 			<PhotoList
 				photoData={props.photoData}
 				isFavorite={isFavorite}
 				toggleFavorite={toggleFavorite}
+				toggleModal={toggleModal}
 			/>
+			{modal && <Modal toggleModal={toggleModal} photoDetail={photoDetail} />}
 		</div>
 	);
 };
