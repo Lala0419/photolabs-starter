604857d10a5372b2724d9676e72c62416983b43f
Nanaya <nanayamiki@gmail.com>
2023-08-21 18:21:46 -0700

    move modal component to HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,17 @@ import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
 import PhotoData from "./mocks/photos";
 import TopicData from "./mocks/topics";
-import Modal from "./routes/PhotoDetailsModal";
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-	const [modal, setModal] = useState(false);
-
-	const toggleModal = () => {
-		setModal((prev) => !prev);
-	};
-
 	return (
 		<div className="App">
 			<HomeRoute
 				photoData={PhotoData}
 				topicData={TopicData}
-				setModal={toggleModal}
+				// setModal={toggleModal}
+				//modal={Modal}
 			/>
-			{modal && <Modal setModal={toggleModal} />}
 		</div>
 	);
 };
