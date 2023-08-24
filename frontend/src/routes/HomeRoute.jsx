b7d4e532be1cde740b4cf2504b00fb59e74dff1e
Nanaya <nanayamiki@gmail.com>
2023-08-23 18:47:16 -0700

    move all the states and logic and pass them down as props

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,41 +5,45 @@ import PhotoList from "components/PhotoList";
 import Modal from "../routes/PhotoDetailsModal";
 
 const HomeRoute = (props) => {
-	const [isFavorite, setIsFavorite] = useState([]);
-	const [modal, setModal] = useState(false);
-	const [photoDetail, setPhotoDetail] = useState({});
+	console.log("propsH", props);
+	// const [isFavorite, setIsFavorite] = useState([]);
+	// const [modal, setModal] = useState(false);
+	// const [photoDetail, setPhotoDetail] = useState({});
 
-	const toggleFavorite = (photoId) => {
-		console.log("photoId", photoId);
-		if (isFavorite.includes(photoId)) {
-			setIsFavorite(isFavorite.filter((id) => id !== photoId));
-		} else {
-			setIsFavorite([...isFavorite, photoId]);
-		}
-		console.log("isFavorite", isFavorite);
-	};
+	// const toggleFavorite = (photoId) => {
+	// 	console.log("photoId", photoId);
+	// 	if (isFavorite.includes(photoId)) {
+	// 		setIsFavorite(isFavorite.filter((id) => id !== photoId));
+	// 	} else {
+	// 		setIsFavorite([...isFavorite, photoId]);
+	// 	}
+	// 	console.log("isFavorite", isFavorite);
+	// };
 
-	const toggleModal = (photoId) => {
-		setModal((prev) => !prev);
-		setPhotoDetail(props.photoData[photoId - 1]);
-		console.log("photoDetail", props.photoData[photoId - 1]);
-	};
+	// const toggleModal = (photoId) => {
+	// 	setModal((prev) => !prev);
+	// 	setPhotoDetail(props.photoData[photoId - 1]);
+	// 	console.log("photoDetail", props.photoData[photoId - 1]);
+	// };
 
 	return (
 		<div className="home-route">
-			<TopNavigation topicData={props.topicData} isFavorite={isFavorite} />
+			<TopNavigation
+				topicData={props.topicData}
+				isFavorite={props.state.isFavorite}
+			/>
 			<PhotoList
 				photoData={props.photoData}
-				isFavorite={isFavorite}
-				toggleFavorite={toggleFavorite}
-				toggleModal={toggleModal}
+				isFavorite={props.state.isFavorite}
+				toggleFavorite={props.toggleFavorite}
+				toggleModal={props.toggleModal}
 			/>
-			{modal && (
+			{props.state.modal && (
 				<Modal
-					toggleModal={toggleModal}
-					photoDetail={photoDetail}
-					toggleFavorite={toggleFavorite}
-					isFavorite={isFavorite}
+					toggleModal={props.toggleModal}
+					photoDetail={props.state.photoDetail}
+					toggleFavorite={props.toggleFavorite}
+					isFavorite={props.state.isFavorite}
 				/>
 			)}
 		</div>
