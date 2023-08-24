8175896006fdb6a19338b1f1452b9498ef9152a8
Nanaya <nanayamiki@gmail.com>
2023-08-23 19:40:42 -0700

    set to create useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -5,14 +5,37 @@ import PhotoData from "../mocks/photos";
 
 export default function useApplicationData() {
 	const [state, setState] = useState(initialState); //global state
-	// const [isFavorite, setIsFavorite] = useState([]);
-	// const [modal, setModal] = useState(false);
-	// const [photoDetail, setPhotoDetail] = useState({});
+	/** const [isFavorite, setIsFavorite] = useState([]);
+	const [modal, setModal] = useState(false);
+	const [photoDetail, setPhotoDetail] = useState({});
+*/
+	// /* insert app levels actions below */
+	// export const ACTIONS = {
+	//   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+	//   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+	//   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+	//   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+	//   SELECT_PHOTO: 'SELECT_PHOTO',
+	//   DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+	// }
 
+	// function reducer(state, action) {
+	//   switch (action.type) {
+	//     case FAV_PHOTO_ADDED:
+	//       return
+	//     { /* insert all relevant actions as case statements*/ }
+	//     }
+	//     default:
+	//       throw new Error(
+	//         `Tried to reduce with unsupported action type: ${action.type}`
+	//       );
+	//   }
+	// }
 	const toggleFavorite = (photoId) => {
 		console.log("photoId", photoId);
 		if (state.isFavorite.includes(photoId)) {
 			//setIsFavorite(isFavorite.filter((id) => id !== photoId));
+
 			setState((prev) => {
 				return {
 					...prev,
