6a33c5164795f95dfd58274c1a20587e73f6f6fa
Nanaya <nanayamiki@gmail.com>
2023-08-25 18:41:48 -0700

    remove useState and add useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,64 +1,82 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 const initialState = { isFavorite: [], modal: false, photoDetail: {} };
 import TopicData from "../mocks/topics";
 import PhotoData from "../mocks/photos";
 
+export const ACTIONS = {
+	FAV_PHOTO_ADDED: "FAV_PHOTO_ADDED",
+	FAV_PHOTO_REMOVED: "FAV_PHOTO_REMOVED",
+	// SET_PHOTO_DATA: "SET_PHOTO_DATA",
+	// SET_TOPIC_DATA: "SET_TOPIC_DATA",
+	SELECT_PHOTO: "SELECT_PHOTO",
+	// DISPLAY_PHOTO_DETAILS: "DISPLAY_PHOTO_DETAILS",
+};
+
+function reducer(state, action) {
+	console.log("action", action);
+	switch (action.type) {
+		case ACTIONS.FAV_PHOTO_ADDED:
+			return {
+				...state,
+				isFavorite: [...state.isFavorite, action.photoId],
+			};
+		case ACTIONS.FAV_PHOTO_REMOVED:
+			return {
+				...state,
+				isFavorite: state.isFavorite.filter((id) => id !== action.photoId),
+			};
+		case ACTIONS.SELECT_PHOTO:
+			return {
+				...state,
+				modal: !state.modal,
+				photoDetail: PhotoData[action.photoId - 1],
+			};
+		default:
+			throw new Error(
+				`Tried to reduce with unsupported action type: ${action.type}`
+			);
+	}
+}
+
 export default function useApplicationData() {
-	const [state, setState] = useState(initialState); //global state
+	const [state, dispatch] = useReducer(reducer, initialState);
+	//const [state, setState] = useState(initialState); //global state
 	/** const [isFavorite, setIsFavorite] = useState([]);
 	const [modal, setModal] = useState(false);
 	const [photoDetail, setPhotoDetail] = useState({});
 */
 	// /* insert app levels actions below */
-	// export const ACTIONS = {
-	//   FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
-	//   FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
-	//   SET_PHOTO_DATA: 'SET_PHOTO_DATA',
-	//   SET_TOPIC_DATA: 'SET_TOPIC_DATA',
-	//   SELECT_PHOTO: 'SELECT_PHOTO',
-	//   DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
-	// }
 
-	// function reducer(state, action) {
-	//   switch (action.type) {
-	//     case FAV_PHOTO_ADDED:
-	//       return
-	//     { /* insert all relevant actions as case statements*/ }
-	//     }
-	//     default:
-	//       throw new Error(
-	//         `Tried to reduce with unsupported action type: ${action.type}`
-	//       );
-	//   }
-	// }
 	const toggleFavorite = (photoId) => {
 		console.log("photoId", photoId);
 		if (state.isFavorite.includes(photoId)) {
 			//setIsFavorite(isFavorite.filter((id) => id !== photoId));
-
-			setState((prev) => {
-				return {
-					...prev,
-					isFavorite: state.isFavorite.filter((id) => id !== photoId),
-				};
-			});
+			dispatch({ type: "FAV_PHOTO_REMOVED", photoId: photoId });
+			// setState((prev) => {
+			// 	return {
+			// 		...prev,
+			// 		isFavorite: state.isFavorite.filter((id) => id !== photoId),
+			// 	};
+			// });
 		} else {
+			dispatch({ type: "FAV_PHOTO_ADDED", photoId: photoId });
 			//setIsFavorite([...isFavorite, photoId]);
-			setState((prev) => {
-				return { ...prev, isFavorite: [...state.isFavorite, photoId] };
-			});
+			// setState((prev) => {
+			// 	return { ...prev, isFavorite: [...state.isFavorite, photoId] };
+			// });
 		}
 	};
 
 	const toggleModal = (photoId) => {
+		dispatch({ type: "SELECT_PHOTO", photoId: photoId });
 		//setModal((prev) => !prev);
-		setState((prev) => {
-			return {
-				...prev,
-				modal: !prev.modal,
-				photoDetail: PhotoData[photoId - 1],
-			};
-		});
+		// setState((prev) => {
+		// 	return {
+		// 		...prev,
+		// 		modal: !prev.modal,
+		// 		photoDetail: PhotoData[photoId - 1],
+		// 	};
+		// });
 	};
 
 	return {
