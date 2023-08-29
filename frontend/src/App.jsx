51255fc8dc03eebc00fb1caf8f9b275e926600a0
Nanaya <nanayamiki@gmail.com>
2023-08-28 21:35:31 -0700

    add props for selecting topic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,18 @@ import React from "react";
 import useApplicationData from "hooks/useApplicationData";
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
-// import PhotoData from "./mocks/photos";
-// import TopicData from "./mocks/topics";
 
-// Note: Rendering a single component to build components in isolation
 const App = (props) => {
-	const { state, toggleFavorite, toggleModal } = useApplicationData();
+	const { state, toggleFavorite, toggleModal, selectTopic } =
+		useApplicationData();
 
 	return (
 		<div className="App">
 			<HomeRoute
 				state={state}
-				// photoData={photoData}
-				// topicData={topicData}
 				toggleFavorite={toggleFavorite}
 				toggleModal={toggleModal}
+				selectTopic={selectTopic}
 			/>
 		</div>
 	);
