7efee18459c4a4cb865e913e3dd5586f19d7146f
Nanaya <nanayamiki@gmail.com>
2023-08-28 21:38:22 -0700

    add onClick func and pass id as a props

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,9 +3,12 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-	//console.log("props", props);
+	const clickHandler = () => {
+		props.selectTopic(props.id);
+	};
+
 	return (
-		<div className="topic-list__item">
+		<div className="topic-list__item" onClick={clickHandler}>
 			<span>{props.title}</span>
 		</div>
 	);
